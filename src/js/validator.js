36c4401d19feec745b785298b9d450b8b9ea61fa
oligo <oligo83@gmail.com>
2025-03-06 01:31:43 +0700

    fix: excess

--- src/js/validator.js
+++ src/js/validator.js
@@ -2,6 +2,6 @@ export default class Validator {
     validateUsername(username) {
             return !/[^a-z0-9-_]/i.test(username)
                 && !/\d{3}/.test(username)
-                && !/(^[\d-_]+)|([\d-_]+$)/.test(username)
+                && !/(^[\d-_])|([\d-_]$)/.test(username)
     }
 }
